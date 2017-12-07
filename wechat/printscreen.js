8cd1c9fa01ea4bb24a96ea218ca284ed011f7709
yaoyaodu <yaoyaodu@YYBTs-mac.local>
2017-12-07 11:17:25 +0800

    tij

--- wechat/printscreen.js
+++ wechat/printscreen.js
@@ -13,40 +13,40 @@ var ScreenShots = function(){
  * 自动回复 配置
  */
 ScreenShots.prototype.xxxx = function(req,res){
-    // var that = this;
-    // var body = req.body;
+    var that = this;
+    var body = req.body;
     // console.log(body);
     return new Promise(function(resolve,reject){
      
-        resolve('123');
-    // (async function() {
-    //     const instance = await phantom.create();
-    //     const page = await instance.createPage();
+        // resolve('123');
+    (async function() {
+        const instance = await phantom.create();
+        const page = await instance.createPage();
 
-    //     const status = await page.open(body.url);
+        const status = await page.open(body.url);
 
-    //     var imageName = new Date().getTime()+'.png';
+        var imageName = new Date().getTime()+'.png';
 
-    //     await page.render(imageName);
-    //     if (status == 'success'){
-    //         console.log(status);
-    //         await instance.exit();
-    //     }
-    //     fs.readFile('./'+imageName, function (err, data) {
-    //         if (err)    return console.log(err);
-    //             // console.log('异步读取：' + data.toString());
-    //             resolve(data);
+        await page.render(imageName);
+        if (status == 'success'){
+            console.log(status);
+            await instance.exit();
+        }
+        fs.readFile('./'+imageName, function (err, data) {
+            if (err)    return console.log(err);
+                // console.log('异步读取：' + data.toString());
+                resolve(data);
 
-    //         fs.unlink('./'+imageName, function (err) {
-    //                  if (err) return console.log(err);
-    //                   console.log('文件删除成功');
-    //         })
-    //     })
-    //   //   const content = await page.property('content');
-    //   //   console.log(content);
+            fs.unlink('./'+imageName, function (err) {
+                     if (err) return console.log(err);
+                      console.log('文件删除成功');
+            })
+        })
+      //   const content = await page.property('content');
+      //   console.log(content);
         
       
-    //   })();
+      })();
     });
 }
 
