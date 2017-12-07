3a25df105300239f83a30e93396021be1f3c7506
yaoyaodu <yaoyaodu@YYBTs-mac.local>
2017-12-07 10:35:34 +0800

    tij

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'), //express 框架 
       wechat  = require('./wechat/wechat'), 
-      screenshots  = require('./wechat/printscreen'), 
+      dd  = require('./wechat/printscreen'), 
       config = require('./config');//引入配置文件
       bodyParser = require('body-parser')
 
